f4febca6e303d884558deedd58479b4feeb5ecc3
David Haines <dlhaines@umich.edu>
2017-06-23 12:07:14 -0400

    TLSPE-3: code review comments

--- src/test/java/edu/umich/ctools/esb/utils/RunTest.java
+++ src/test/java/edu/umich/ctools/esb/utils/RunTest.java
@@ -26,21 +26,7 @@ public class RunTest {
 		in.close();
 		return props;
 	}
-
-//	// TODO: extract out method to pull in grouped properties and place in map.
-//	// should be property file independent
-//	//take input of Props, list of property names, name of group, 
-//	//loop through and return as hashmap
-//			
-//	public static HashMap<String,String> getPropertiesGroup	 (Properties props, String group, List<String> propertyNames) {
-//		HashMap<String, String> value = new HashMap<String, String>();
-//		String key = "tokenServer";
-//		propertyNames.add(key);
-//		
-//		value.put(key, props.getProperty(group + key));
-//		return value;
-//	}
-//	
+	
 	public static void main(String [] args) throws UnirestException, IOException{
 		RunTest runTest = new RunTest();
 		
